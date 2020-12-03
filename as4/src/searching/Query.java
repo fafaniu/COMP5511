495c1cb69f57b675470fe18c915eea71809082a6
tianhaoxie <52686796+tianhaoxie@users.noreply.github.com>
2020-12-03 19:28:43 +0800

    Add files via upload

--- as4/src/searching/Query.java
+++ as4/src/searching/Query.java
@@ -1,5 +1,19 @@
 package searching;
+/*	 Important note about the input Excel file :
+first column ID
+second column latitude
+third column longitude
+fourth column name
 
+Important note about the query file:
+To find what at a certain coordinate:
+"What is at latitude and longitude:44.00,-158.00" (between ':' and coordinate , do not have spaces)
+To retrieve with ID:
+"Find the details of:AAAAA" (between ':' and ID , do not have spaces)
+To retrieve with name:
+"Find all:Lake"(between ':' and name , do not have spaces)
+
+*/
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -9,8 +23,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class Query {
 	public static void main(String[] argv){
+		System.out.println("Please wait, the program will run for several minutes.  The searching results are in 'data/searchingresult.txt'");
         File DataFile = new File("data/cgn_canada_csv_eng(unsorted).csv");  
         File queryFile = new File("/Users/xietianhao/eclipse-workspace/as4/data/query.txt");
         File outFile = new File("data/searchingresult.txt");
@@ -19,9 +35,9 @@ public class Query {
         String query1="What is at latitude and longitude:";
         String query2="Find the details of:";
         String query3="Find all:";
-        
+        ArrayList<Index> Indexs = new ArrayList<>();
         ArrayList<String> data= new ArrayList<>();
-        
+        ArrayList<String> normal_word=new ArrayList<>();
         try {
             BufferedReader DataReader = new BufferedReader(new FileReader(DataFile));
             BufferedReader QueryReader = new BufferedReader(new FileReader(queryFile));
@@ -96,7 +112,8 @@ public class Query {
                     }
             	}
             	
-            	//retrieving with GeoName(hash table)
+            	//retrieving with GeoName(inverted index)
+            	
             	if (QueryString.startsWith(query3) ) {
             		for (int i= query3.length();i<QueryString.length();i++) {
             			
@@ -104,6 +121,55 @@ public class Query {
             			
             		}
             		writer.write("\nFind all: "+ sb.toString()+"\nOutput:\n");
+            		String[] name=new String[a.length];
+            		for (int i=0;i<a.length;i++) {
+                    	name[i]=Cut.GeoName(a[i]);
+                    }
+            		
+            		for (int i=0;i<name.length;i++) {
+            			String[] words = name[i].split(" ");
+            			for (int j=0;j<words.length;j++) {
+            				if(words[j].equals("aux")|| words[j].equals("des") || words[j].equals("du")|| words[j].equals("la")|| words[j].equals("La")|| words[j].equals("sur")){
+        	    				continue;
+        	    			}
+        	    			else if (normal_word.contains(words[j])) {
+        	    				
+        	    				int n = normal_word.indexOf(words[j]);	
+        	    				 //System.out.println(word);
+        	    				Indexs.get(n).setID(a[i]);
+        	    				Indexs.get(n).setName(a[i]);
+        	    				Indexs.get(n).setLatitude(a[i]);
+        	    				Indexs.get(n).setLongitude(a[i]);
+        	    				Indexs.get(n).setrecord(i);  	    				
+        	    			}
+        	    			
+        	    			else {
+        	    			normal_word.add(words[j]);
+        	    			Indexs.add(new Index(a[i], words[j],i));
+        	    			}
+            				
+        	    		}
+            			
+        	    	
+        	    	
+            			}
+            		for (int i=0;i<normal_word.size();i++) {
+            			if (normal_word.get(i).contains(sb.toString())) {
+            				int m=i;
+            			
+            		
+            				writer.write("Index of the Searching word " + m +"\n");
+            				writer.write("Geographic ID: " + Indexs.get(m).getID()+"\n");
+            				writer.write("Geographic Name: " + Indexs.get(m).getName()+"\n");
+            				writer.write("Latitude: " + Indexs.get(m).getLatitude()+"\n");
+            				writer.write("Longitude: " + Indexs.get(m).getLongitude()+"\n");
+            			}
+            		
+            	}
+            		
+            		}
+            				
+            		/*
             		Hashing hashing=new Hashing();
             		hashing.establish_location(a);
                     int[] result = hashing.searching_location(sb.toString());
@@ -117,36 +183,19 @@ public class Query {
                     		String longitude=Cut.Longitude(a[result[i]]);
                     		writer.write("\nGeographic ID: "+ GeoID+"\nlatitude: " +latitude+"\nlongitude: "+longitude+"\n");
                     }
+                    
                     }
-            	}
+            	    */
+            	
+            	
             }
+            
             
             QueryReader.close();
             writer.close();
             
             
-/*            
-// retrieving with ID, by BST            
-            BST tree= new BST();
-            tree.establish(a);
-            int out1=tree.searching("EKOMY");
-            writer.write(tree.printout(out1, a));
-// retrieving with coordinate, by open hashing
-            Hashing hashing = new Hashing();
-            hashing.establish_coordinate(a);
-            int[] result= hashing.searching_coordinate("56.79,-61.22");
-            
-            writer.write(hashing.printout_coordinate(result, a));
-// retrieving with location
-            hashing.establish_location(a);
-            int[] result1 = hashing.searching_location("Albert Lake");
-            writer.write(hashing.printout_location(result1, a));
-//Inverted index
-            Inverted_index nnw= new Inverted_index();
-            nnw.establish(a);
-            nnw.printindex(a);
-            writer.close();
-            */
+
         } catch (FileNotFoundException ex) {
             System.out.println("Not finding file!");
         } catch (IOException ex) {
--- as4/src/searching/test.java
+++ as4/src/searching/test.java
@@ -1,6 +1,7 @@
 package searching;
 
 import java.io.BufferedReader;
+import java.util.Scanner;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,41 +9,293 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class test {
-	public static void main(String[] argv) {
-		File DataFile = new File("data/cgn_canada_csv_eng(unsorted).csv");
-		File outFile = new File("data/sorting.txt");
-		ArrayList<String> data= new ArrayList<>();
-		String DataString = "";
-        try {
-            BufferedReader DataReader = new BufferedReader(new FileReader(DataFile));
-            
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
-            if (!outFile.exists()) {
-				 outFile.createNewFile();
-			 }
-            while((DataString = DataReader.readLine())!= null){
-                data.add(DataString);
-            }
-            DataReader.close();
-            int dataSize=data.size();
-            
-            String[] a= (String[]) data.toArray(new String[dataSize]);
-            String[] ID= new String[a.length];
-            for (int i=0;i<a.length;i++) {
-            	ID[i]=Cut.ID(a[i]);
-            }
-            Sort sid=new Sort(ID);
-            sid.mergesort(sid.in, sid.index, sid.temp, sid.left, sid.right);
-            for (int i=0;i<a.length;i++) {
-            	writer.write(a[sid.index[i]]+"\n");
-            }
-            writer.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println("Not finding file!");
-        } catch (IOException ex) {
-            System.out.println("error");
-        }
+	public static void main(String[] args)throws Exception {
+		System.out.println("Program is loading, please wait the message (enter the name of geographic) pop out");
+ 		ArrayList<Index> Indexs = new ArrayList<>();
+ 		ArrayList<String> Indexss = new ArrayList<>();
+ 		String encoding="UTF-8"; 		
+	    File file = new File("data/cgn_canada_csv_eng(unsorted).csv");
+	    InputStreamReader read = new InputStreamReader(new FileInputStream(file),encoding);
+	    BufferedReader br = new BufferedReader(read); 	    
+	    String data; 
+	    int j=0;
+	    int m=0;
+	    
+	    
+	    while ((data = br.readLine()) != null) {
+	    	//Indexs.add(new Index(data));
+	    	 
+	    	String s = readname(data);
+	    	//Indexss.add(s);
+	    	String[] words = s.split(" ");
+	
+	    	for(int i=0; i<words.length; i++) {
+	    		
+	    		String word = words[i];
+	    		
+	    			if(word.equals("aux")|| word.equals("des") || word.equals("du")|| word.equals("la")|| word.equals("La")|| word.equals("sur")){
+	    				// System.out.println("aux");
+	    			}
+	    			else if (Indexss.contains(word)) {
+	    				
+	    				int n = Indexss.indexOf(word);	
+	    				 //System.out.println(word);
+	    				Indexs.get(n).setID(data);
+	    				Indexs.get(n).setName(data);
+	    				Indexs.get(n).setLatitude(data);
+	    				Indexs.get(n).setLongitude(data);
+	    				Indexs.get(n).setrecord(j);  	    				
+	    			}
+	    			//else if (Indexss.contains(word)) {	    	    			}
+	    			
+	    			//if (Indexss.contains(words[i])) {}
+	    			else {
+	    			Indexss.add(word);
+	    			Indexs.add(new Index(data, word,j));
+	    			}
+	    		}
+	    	
+	    	j++;
+	    	
+	    	
+	    }
+	    
+	    
+	   
+	    
+	
+	    
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter the name of geographic for the searching | For Exit: please enter 'n'");
+		//String search = keyboard.nextLine();
+		
+	    while (true) {
+	    	String search = keyboard.nextLine();
+	    	
+        	if (search.equals("n")) {
+        		System.out.println("Program Exit"); 
+        		break;
+        	}
+        	else {
+        		
+        		m = Indexss.indexOf(search);
+        		/*for(int i = 0; i< Indexss.size(); i++) {
+        			if (Indexss.contains(search)) {
+        				m = i;
+        			
+        			}
+        		} doesnot work well
+        		*/
+				System.out.println("Index of the Searching word" + m);
+			    System.out.println("Geographic ID: " + Indexs.get(m).getID());
+    	    	System.out.println("Geographic Name: " + Indexs.get(m).getName());
+    	    	System.out.println("Latitude: " + Indexs.get(m).getLatitude());
+    	    	System.out.println("Longitude: " + Indexs.get(m).getLongitude());
+        		}
+       
+	    }
+		
+		
+		
+		/*
+		int m = Indexss.indexOf(search);
+		System.out.println(m);
+		
+	    System.out.println("Geographic ID: " + Indexs.get(m).getID());
+    	System.out.println("Geographic Name: " + Indexs.get(m).getName());
+    	System.out.println("Latitude: " + Indexs.get(m).getLatitude());
+    	System.out.println("Longitude: " + Indexs.get(m).getLongitude());
+		*/
+		
+		 
+		  
+}
+
+public static String readname(String k) {
+int j=0;
+StringBuffer sb= new StringBuffer();
+
+for (int i=0;i<k.length();i++) {
+if (k.charAt(i)==',') {
+	j++;
+}
+if (j>=3) {
+	if (k.charAt(i)!=',') {
+		sb.append(k.charAt(i));
+	}
+}
+}
+return sb.toString();
+}
+	    
+	    
+	    
+	
+}	    
+
+class Index {
+	
+	public String Word;
+	ArrayList<String> ID = new ArrayList<>();
+	ArrayList<String> Latitude = new ArrayList<>();
+	ArrayList<String> Longitude = new ArrayList<>();
+	ArrayList<String> Name = new ArrayList<>();
+	ArrayList<Integer> record = new ArrayList<>();
+	
+	
+	
+	public Index(String k, String x, int j) {
+		ID.add(readid(k));
+		Word = x;
+		Name.add(readname(k));
+		Latitude.add(readlatitude(k));
+		Longitude.add(readlongitude(k));
+		record.add(j);
+	}
+	
+	
+	/*
+	
+	public Index(String k) {
+		this.ID = readid(k);
+		this.Name = readname(k);
+		this.Latitude = readlatitude(k);
+		this.Longitude = readlongitude(k);				
+	}
+	
+	
+	
+	public String getID(){
+		return ID;
+	}
+	public String getName(){
+		return Name;
+	}
+	public String getLatitude(){
+		return Latitude;
+	}
+	public String getLongitude(){
+		return Longitude;
+	}
+	*/
+	public ArrayList<String> getID(){
+		return ID;
+	}
+	public ArrayList<String> getName(){
+		return Name;
+	}
+	public ArrayList<String> getLatitude(){
+		return Latitude;
+	}
+	public ArrayList<String> getLongitude(){
+		return Longitude;
+		}
+	public String getWord(){
+		return Word;
+		}
+	
+	public ArrayList<Integer> getrecord(){
+			return record;
+		}
+	
+	
+	
+	
+	public void setID (String x) {	
+		x = readid(x);
+		ID.add(x);	
+	}
+	
+	public void setLatitude (String x) {	
+		x = readlatitude(x);
+		Latitude.add(x);	
+	}
+	public void setLongitude (String x) {	
+		x = readlongitude(x);
+		Longitude.add(x);	
+	}
+	public void setName (String x) {	
+		x = readname(x);
+		Name.add(x);	
+	}
+	
+	public void setrecord (int j) {	
+		record.add(j);	
+	}
+			
+	
+	private String readid(String line) {
+		int i=0;
+		StringBuffer sb =new StringBuffer();
+		while (line.charAt(i)!=','){
+			sb.append(line.charAt(i));
+			i++;
+		}
+		return sb.toString();
+	}
+	
+
+	private String readname(String k) {
+		int j=0;
+		StringBuffer sb= new StringBuffer();
+		
+		for (int i=0;i<k.length();i++) {
+			if (k.charAt(i)==',') {
+				j++;
+			}
+			if (j>=3) {
+				if (k.charAt(i)!=',') {
+					sb.append(k.charAt(i));
+				}
+			}
+		}
+		return sb.toString();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private String readlatitude(String k) {
+		int j=0;
+		StringBuffer sb= new StringBuffer();
+		
+		for (int i=0;i<k.length();i++) {
+			if (k.charAt(i)==',') {
+				j++;
+			}
+			if (j==1) {
+				if (k.charAt(i)!=',') {
+					sb.append(k.charAt(i));
+				}
+			}
+		}
+		return sb.toString();
+	}
+
+	
+	private String readlongitude(String k) {
+		int j=0;
+		StringBuffer sb= new StringBuffer();
+		
+		for (int i=0;i<k.length();i++) {
+			if (k.charAt(i)==',') {
+				j++;
+			}
+			if (j==2) {
+				if (k.charAt(i)!=',') {
+					sb.append(k.charAt(i));
+				}
+			}
+		}
+		return sb.toString();
 	}
 }
